986523ed6c2722506d983ac2998e1827b4c8cb47
Hrithik Shah <hrithik21.shah@gmail.com>
2024-11-13 19:10:58 -0500

    Create GameUser and Assets entities

---
+++ src/main/java/entity/Assets.java
@@ -0,0 +1,40 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class Assets {
+    private final int home;
+    private final ArrayList<Stock> stocks;
+    private final int cash;
+    private final int car;
+
+    public Assets() {
+        home = 0;
+        stocks = new ArrayList<Stock>();
+        cash = 0;
+        car = 0;
+    }
+
+    public Assets(int home, ArrayList<Stock> stocks, int cash, int car) {
+        this.home = home;
+        this.stocks = stocks;
+        this.cash = cash;
+        this.car = car;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public ArrayList<Stock> getStocks {
+        return stocks;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public int getCar() {
+        return car;
+    }
+}
--- src/main/java/entity/GameUser.java
+++ src/main/java/entity/GameUser.java
@@ -1,4 +1,87 @@
 package entity;
 
-public class GameUser {
+import java.util.ArrayList;
+
+public class GameUser implements User{
+    private final String name
+    private final String password;
+
+    private String characterName;
+    private Avatar avatar;
+    private int happiness;
+    private int qualiftyOfLife;
+    private Assets assets;
+    private Liabilities liabilities;
+    private ArrayList<Decision> decisions;
+
+    public GameUser(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public Avatar getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Avatar avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public int getQualiftyOfLife() {
+        return qualiftyOfLife;
+    }
+
+    public void setQualiftyOfLife(int qualiftyOfLife) {
+        this.qualiftyOfLife = qualiftyOfLife;
+    }
+
+    public Assets getAssets() {
+        return assets;
+    }
+
+    public void setAssets(Assets assets) {
+        this.assets = assets;
+    }
+
+    public Liabilities getLiabilities() {
+        return liabilities;
+    }
+
+    public void setLiabilities(Liabilities liabilities) {
+        this.liabilities = liabilities;
+    }
+
+    public ArrayList<Decision> getDecisions() {
+        return decisions;
+    }
+
+    public void setDecisions(ArrayList<Decision> decisions) {
+        this.decisions = decisions;
+    }
 }
