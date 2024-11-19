f7b18726cfa9ba940a8a9bd0e531be7ba59c81b6
Hrithik Shah <hrithik21.shah@gmail.com>
2024-11-18 20:41:36 -0500

    Add Nikita's Avatar entitiy

---
+++ src/main/java/entity/Avatar.java
@@ -0,0 +1,38 @@
+package entity;
+
+/**
+ * Represents the player's avatar in the game.
+ */
+public class Avatar {
+    private final String id;
+    private final String characterName;
+    private final String imagePath;
+
+    public Avatar(String id, String characterName, String imagePath) {
+        this.id = id;
+        this.characterName = characterName;
+        this.imagePath = imagePath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public boolean equals(Avatar o) {
+        return id.equals(o.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
+
