0dc9bc1d56ce26910e1353408ad815fc9dc90521
Hrithik Shah <hrithik21.shah@gmail.com>
2024-11-22 20:59:58 -0500

    Add Marina's changes related to Decision entity

--- src/main/java/entity/Decision.java
+++ src/main/java/entity/Decision.java
@@ -5,12 +5,14 @@ import java.time.LocalDateTime;
 public class Decision {
     private final LocalDateTime timestamp;
     private final String decisionText;
+    private final String decisionResponse;
     private final double netWorthChange;
     private final double happinessChange;
 
-    public Decision(LocalDateTime timestamp, String decisionText, double netWorthChange, double happinessChange) {
+    public Decision(LocalDateTime timestamp, String decisionText, String decisionResponse, double netWorthChange, double happinessChange) {
         this.timestamp = timestamp;
         this.decisionText = decisionText;
+        this.decisionResponse = decisionResponse;
         this.netWorthChange = netWorthChange;
         this.happinessChange = happinessChange;
     }
@@ -19,9 +21,9 @@ public class Decision {
         return timestamp;
     }
 
-    public String getDecisionText() {
-        return decisionText;
-    }
+    public String getDecisionText() { return decisionText; }
+
+    public String getDecisionResponse() { return decisionResponse; }
 
     public double getNetWorthChange() {
         return netWorthChange;
