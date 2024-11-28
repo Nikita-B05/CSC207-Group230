8b0a7ad8eae8aac7972ef1a59dc6d6a75bc96e08
PLATA <abhiketan1@gmail.com>
2024-11-27 20:03:06 -0500

    Actually made ChangePasswordView.java look goated

--- src/main/java/view/ChangePasswordView.java
+++ src/main/java/view/ChangePasswordView.java
@@ -1,6 +1,6 @@
 package view;
 
-import java.awt.Component;
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import javax.swing.*;
@@ -30,21 +30,33 @@ public class ChangePasswordView extends JPanel implements PropertyChangeListener
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         newPasswordField = new JPasswordField(15);
+        newPasswordField.setMaximumSize(new Dimension(200, 30)); // Set a fixed size for the password field
 
         changePasswordButton = new JButton("Change Password");
+        changePasswordButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         changePasswordButton.addActionListener(e -> {
             String newPassword = new String(newPasswordField.getPassword());
             changePasswordController.execute(newPassword, changePasswordViewModel.getState().getUsername());
         });
 
         cancelButton = new JButton("Cancel");
+        cancelButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         cancelButton.addActionListener(e -> changePasswordController.switchToSettingsView());
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // Add padding and spacing
+        this.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+
+        this.add(Box.createVerticalGlue());
         this.add(title);
+        this.add(Box.createRigidArea(new Dimension(0, 20)));
         this.add(newPasswordField);
+        this.add(Box.createRigidArea(new Dimension(0, 20)));
         this.add(changePasswordButton);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
         this.add(cancelButton);
+        this.add(Box.createVerticalGlue()); // Add flexible space below the components
 
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
