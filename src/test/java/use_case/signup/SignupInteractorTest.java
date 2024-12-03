20dc0528fabf35f188cbde5d9a8e5013ea000147
Hrithik Shah <hrithik21.shah@gmail.com>
2024-12-03 13:31:13 -0500

    Add homepage test case

--- src/test/java/use_case/signup/SignupInteractorTest.java
+++ src/test/java/use_case/signup/SignupInteractorTest.java
@@ -1,27 +1,44 @@
 package use_case.signup;
 
 import data_access.MongoDBUserDataAccessObject;
+import entity.CommonUser;
 import entity.CommonUserFactory;
 import entity.User;
 import entity.UserFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignupInteractorTest {
+    private static MongoDBUserDataAccessObject userRepository;
+
+    @BeforeAll
+    public static void setUp() {
+        User user = new CommonUser("testing", "password");
+        userRepository = new MongoDBUserDataAccessObject(new CommonUserFactory());
+        userRepository.setCurrentUsername("testing");
+        userRepository.save(user);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        userRepository.deleteUser("testing");
+    }
 
     @Test
     void successTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "password");
-        SignupUserDataAccessInterface userRepository = new MongoDBUserDataAccessObject(new CommonUserFactory());
-
+        userRepository.deleteUser("testing");
+        SignupInputData inputData = new SignupInputData(
+                "testing", "password", "password");
         // This creates a successPresenter that tests whether the test case is as we expect.
         SignupOutputBoundary successPresenter = new SignupOutputBoundary() {
             @Override
             public void prepareSuccessView(SignupOutputData user) {
                 // 2 things to check: the output data is correct, and the user has been created in the DAO.
-                assertEquals("Paul", user.getUsername());
-                assertTrue(userRepository.existsByName("Paul"));
+                assertEquals("testing", user.getUsername());
+                assertTrue(userRepository.existsByName("testing"));
             }
 
             @Override
@@ -31,18 +48,118 @@ class SignupInteractorTest {
 
             @Override
             public void switchToLoginView() {
-                // This is expected
+                fail("Use case success is unexpected.");
             }
         };
 
-        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, new CommonUserFactory());
+        SignupInputBoundary interactor = new SignupInteractor(
+                userRepository, successPresenter, new CommonUserFactory());
         interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
+    }
+
+    @Test
+    void failureUsernameIsEmptyTest() {
+        userRepository.deleteUser("testing");
+        SignupInputData inputData = new SignupInputData(
+                "", "password", "password");
+
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Username cannot be empty.", error);
+            }
+
+            @Override
+            public void switchToLoginView() {
+                fail("Use case success is unexpected.");
+            }
+        };
+
+        SignupInputBoundary interactor =
+                new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
+        interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
+    }
+
+    @Test
+    void failurePasswordIsEmptyTest() {
+        userRepository.deleteUser("testing");
+        SignupInputData inputData = new SignupInputData(
+                "testing", "", "password");
+
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Password cannot be empty.", error);
+            }
+
+            @Override
+            public void switchToLoginView() {
+                fail("Use case success is unexpected.");
+            }
+        };
+
+        SignupInputBoundary interactor =
+                new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
+        interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
+    }
+
+    @Test
+    void failurePasswordTooShortTest() {
+        userRepository.deleteUser("testing");
+        SignupInputData inputData = new SignupInputData(
+                "testing", "1234", "1234");
+
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Password must be at least 8 characters.", error);
+            }
+
+            @Override
+            public void switchToLoginView() {
+                fail("Use case success is unexpected.");
+            }
+        };
+
+        SignupInputBoundary interactor =
+                new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
+        interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
     }
 
     @Test
     void failurePasswordMismatchTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
-        SignupUserDataAccessInterface userRepository = new MongoDBUserDataAccessObject(new CommonUserFactory());
+        userRepository.deleteUser("testing");
+        SignupInputData inputData = new SignupInputData(
+                "testing", "password", "wrong");
 
         // This creates a presenter that tests whether the test case is as we expect.
         SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
@@ -59,23 +176,20 @@ class SignupInteractorTest {
 
             @Override
             public void switchToLoginView() {
-                // This is expected
+                fail("Use case success is unexpected.");
             }
         };
 
         SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
         interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
     }
 
     @Test
     void failureUserExistsTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
-        SignupUserDataAccessInterface userRepository = new MongoDBUserDataAccessObject(new CommonUserFactory());
-
-        // Add Paul to the repo so that when we check later they already exist
-        UserFactory factory = new CommonUserFactory();
-        User user = factory.create("Paul", "pwd");
-        userRepository.save(user);
+        SignupInputData inputData = new SignupInputData(
+                "testing", "password", "wrong");
 
         // This creates a presenter that tests whether the test case is as we expect.
         SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
@@ -92,11 +206,40 @@ class SignupInteractorTest {
 
             @Override
             public void switchToLoginView() {
-                // This is expected
+                fail("Use case success is unexpected.");
             }
         };
 
-        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
+        SignupInputBoundary interactor = new SignupInteractor(
+                userRepository, failurePresenter, new CommonUserFactory());
         interactor.execute(inputData);
+        userRepository.save(new CommonUser("testing", "password"));
+        userRepository.setCurrentUsername("testing");
+    }
+
+    @Test
+    void switchToLoginViewTest() {
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void switchToLoginView() {
+                assertTrue(true);
+            }
+        };
+
+        SignupInputBoundary interactor = new SignupInteractor(
+                userRepository, failurePresenter, new CommonUserFactory());
+        interactor.switchToLoginView();
     }
 }
